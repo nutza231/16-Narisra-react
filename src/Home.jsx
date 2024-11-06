9bc36f4a3cd127a84c3c02452842566ece284606
nutza <www.nutzazaza@gmail.com>
2024-11-06 15:57:14 +0700

    second

--- src/Home.jsx
+++ src/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
       <main className="max-w-7xl mx-auto px-4 py-12">
         <div className="text-center mb-12">
           <h1 className="text-4xl font-bold mb-2">Generation Thailand</h1>
-          <h2 className="text-3xl font-bold">Home - User Sector</h2>
+          <h2 className="text-3xl font-bold ">Home - User Sector</h2>
         </div>
 
         {/* ปุ่มเลือก Sector */}
